68fd6f5d2224c38d01c096592989bc32143e4f96
bwgjoseph <josephgan@live.com.sg>
2023-02-11 01:28:46 +0800

    sync-up with gitlab instance

--- project/search/src/main/java/com/bwgjoseph/search/SearchApplication.java
+++ project/search/src/main/java/com/bwgjoseph/search/SearchApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SearchApplication {
 
 	public static void main(String[] args) {
+        // disable workflow
+        // after create mr
 		SpringApplication.run(SearchApplication.class, args);
 	}
 
